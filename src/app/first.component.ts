9c8893072d4405a3e978539a333da23017c71625
Lars Bergqvist <lars.g.bergqvist@gmail.com>
2021-07-28 12:04:15 +0200

    First version of Angular component with routes

---
+++ src/app/first.component.ts
@@ -0,0 +1,7 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "app-first",
+  template: "<div>Content of first component</div>",
+})
+export class FirstComponent {}
---
+++ src/app/second.component.ts
@@ -0,0 +1,7 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "app-second",
+  template: "<div>Content of second component</div>",
+})
+export class SecondComponent {}
